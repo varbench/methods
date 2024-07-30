b5b5f295ff789414c342a7d14d3b65ff8a38efc6
frferra90 <fraferrari1990@gmail.com>
2024-07-30 12:46:52 +0200

    updating chain 20 and 200

--- scripts/Heisenberg/chain_20_P/vmc_gutzwiller.sh
+++ scripts/Heisenberg/chain_20_P/vmc_gutzwiller.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 ../../../programs/vmc_gutzwiller/compile.sh
-../../../programs/vmc_gutzwiller/lattice_1d.x <wf.in >wf.out
-../../../programs/vmc_gutzwiller/preptj_1d.x <datprep.in >prep.out
-../../../programs/vmc_gutzwiller/monte_carlo_tj.x <VMC.in >VMC.out
+
+cd vmc_gutzwiller_inputs
+../../../../programs/vmc_gutzwiller/lattice_1d.x <wf.in >wf.out
+../../../../programs/vmc_gutzwiller/preptj_1d.x <datprep.in >prep.out
+../../../../programs/vmc_gutzwiller/monte_carlo_tj.x <VMC.in >VMC.out
+
 
 
 
