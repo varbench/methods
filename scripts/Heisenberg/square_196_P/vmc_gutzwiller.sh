85012f3df60bddebc2c4718b70cbb0da5cd758d3
frferra90 <fraferrari1990@gmail.com>
2024-07-30 19:06:18 +0200

    added square heis

---
+++ scripts/Heisenberg/square_196_P/vmc_gutzwiller.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#Compile codes
+cd ../../../programs/vmc_gutzwiller
+chmod +x compile.sh
+./compile.sh
+cd -
+
+#Run VMC
+cd vmc_gutzwiller_inputs
+../../../../programs/vmc_gutzwiller/lattice_2d.x <wf.in >wf.out
+../../../../programs/vmc_gutzwiller/preptj_2d.x <datprep.in >prep.out
+../../../../programs/vmc_gutzwiller/monte_carlo_tj.x <VMC.in >VMC.out
+
+#Read data 
+echo "0 10000 5 0" | ../../../../programs/vmc_gutzwiller/read_ene.x >reading  #energy (<H>/N) + error
+echo "<H>/N: $(tail -n 1 fort.20)"
+
+echo "0 10000 5 1" | ../../../../programs/vmc_gutzwiller/read_ene.x >reading #energy**2 (<H**2>/N**2) + error
+echo "<H**2>/N**2: $(tail -n 1 fort.20)"
+
+#Note: statistical errors can be reduced by performing more MC steps
+
+
+
+
+
+
+
+
+
