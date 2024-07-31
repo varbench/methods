4cff5b246249e3af8e556ccf65bf6db3e03894ae
Dian Wu <dian.wu@epfl.ch>
2024-07-31 08:49:22 +0200

    Fix vmc_gutzwiller scripts

--- scripts/Heisenberg/chain_20_P/vmc_gutzwiller.sh
+++ scripts/Heisenberg/chain_20_P/vmc_gutzwiller.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"
+
 #Compile codes
-cd ../../../programs/vmc_gutzwiller
+cd ../../../programs/vmc_gutzwiller || exit
 chmod +x compile.sh
 ./compile.sh
 cd -
 
 #Run VMC
-cd vmc_gutzwiller_inputs
+cd vmc_gutzwiller_inputs || exit
 ../../../../programs/vmc_gutzwiller/lattice_1d.x <wf.in >wf.out
 ../../../../programs/vmc_gutzwiller/preptj_1d.x <datprep.in >prep.out
 ../../../../programs/vmc_gutzwiller/monte_carlo_tj.x <VMC.in >VMC.out
 
-#Read data 
+#Read data
 echo "0 10000 5 0" | ../../../../programs/vmc_gutzwiller/read_ene.x >reading  #energy (<H>/N) + error
 echo "<H>/N: $(tail -n 1 fort.20)"
 
@@ -20,12 +22,3 @@ echo "0 10000 5 1" | ../../../../programs/vmc_gutzwiller/read_ene.x >reading #en
 echo "<H**2>/N**2: $(tail -n 1 fort.20)"
 
 #Note: statistical errors can be reduced by performing more MC steps
-
-
-
-
-
-
-
-
-
