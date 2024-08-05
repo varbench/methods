b2fab6e6bfbd7595553b566009bef35e84406213
ltocchio <87852520+ltocchio@users.noreply.github.com>
2024-08-05 15:48:45 +0200

    Create datprep.d

---
+++ scripts/Hubbard/square_64_P_25_-8/FN/datprep.d
@@ -0,0 +1,38 @@
+&symmetries
+iesrif=0
+ioptr=0
+iflagdxy=0
+ibc=0
+/
+&couplings
+U=8.d0
+t(1)=1.d0
+t(2)=1.d0
+/
+&nelectrons
+nelup=25
+neldo=39
+/
+&wavefunction
+iesd=14
+iback(1)=1
+iback(2)=1
+iback(3)=1
+iback(4)=1
+iesfree=1
+ieskin=1
+alphav(1)=1.d0
+alphav(2)=1.d0
+muf=0.d0
+/
+ 
+  -0.2506367E+01  -0.9324565E-01  -0.1035931E+00  -0.7963980E-01  -0.6484850E-01  -0.4311133E-01  -0.5130674E-01  -0.4380835E-01  -0.2929813E-01  -0.4342962E-01  -0.3761552E-01  -0.1579993E-01  -0.2426534E-01  -0.8847130E-02 
+ ! Jastrow HD
+  0.5927008E+00   !  back1
+  0.6518492E-02    ! back2
+   0.1396886E+00      ! back3
+   0.1007132E+01 ! back4
+   0.3377389E-01    ! d-wave
+                       ! s-wave
+       ! AF
+  -0.1930320E+00  ! deltamutilde 
