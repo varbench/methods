b14b94d5b66fe5a4259044315fb09aa25482c571
ltocchio <87852520+ltocchio@users.noreply.github.com>
2024-08-05 15:36:49 +0200

    Create datprep.d

---
+++ scripts/Hubbard/square_64_P_28_-8/VMC/datprep.d
@@ -0,0 +1,38 @@
+&symmetries
+iesrif=0
+ioptr=0
+iflagdxy=0
+ibc=0
+/
+&couplings
+U=8.d0
+t(1)=1.d0
+t(2)=1.d0
+/
+&nelectrons
+nelup=28
+neldo=36
+/
+&wavefunction
+iesd=14
+iback(1)=1
+iback(2)=1
+iback(3)=1
+iback(4)=1
+iesfree=1
+ieskin=1
+alphav(1)=1.d0
+alphav(2)=1.d0
+muf=0.d0
+/
+ 
+  -0.2895263E+01  -0.9214406E-01  -0.1184024E+00  -0.8765692E-01  -0.6612915E-01  -0.3720650E-01  -0.4090031E-01  -0.3267212E-01  -0.1831888E-01  -0.2869258E-01  -0.2274503E-01  -0.7512538E-02  -0.1216396E-01  -0.3351652E-02 
+ ! Jastrow HD
+  0.1044102E+01   !  back1
+  0.6528359E-01    ! back2
+   0.2489132E+00      ! back3
+   0.1016162E+01 ! back4
+   0.1232511E+00    ! d-wave
+                       ! s-wave
+       ! AF
+  -0.1432589E+00  ! deltamutilde
