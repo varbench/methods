87b26cafe3512c2f20ed83a2cb7b6a9a8b966c84
ltocchio <87852520+ltocchio@users.noreply.github.com>
2024-08-05 15:19:55 +0200

    Create fn_hubbard.sh

---
+++ scripts/Hubbard/square_64_P_32_-4/FN/fn_hubbard.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#Compile codes
+cd ../../../programs/vmc_hubbard || exit
+chmod +x compile.sh
+./compile.sh
+cd -
+
+#Run VMC
+../../../../programs/vmc_hubbard/Prep_uniform_wf.x < datprep.d > prep.out
+../../../../programs/vmc_hubbard/Main_uniform_wf.x < datasfn.d > FN.out
+
+#Read data
+
+echo "30 500 2 0" | ../../../../programs/vmc_hubbard/Energy.x  #energy (<H>/N) + error
+echo "<H>/N: $(tail -n 1 fort.30)"
+
+#Note: statistical errors can be reduced by performing more MC steps
