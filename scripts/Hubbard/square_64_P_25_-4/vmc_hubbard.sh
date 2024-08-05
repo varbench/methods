34b057e129386e5a6f6e79963052b462963b3649
ltocchio <87852520+ltocchio@users.noreply.github.com>
2024-08-05 13:58:48 +0200

    Update vmc_hubbard.sh

--- scripts/Hubbard/square_64_P_25_-4/vmc_hubbard.sh
+++ scripts/Hubbard/square_64_P_25_-4/vmc_hubbard.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cd "$(dirname "$0")"
-
 #Compile codes
 cd ../../../programs/vmc_hubbard || exit
 chmod +x compile.sh
@@ -9,15 +7,17 @@ chmod +x compile.sh
 cd -
 
 #Run VMC
-cd vmc_hubbard_inputs || exit
-../../../../programs/vmc_hubbard/Prep_stripes.x <datprep.d >prep.out
-../../../../programs/vmc_hubbard/Main_stripes.x <datasvmc.d >VMC.out
+../../../../programs/vmc_hubbard/Prep_stripes.x < datprep.d > prep.out
+../../../../programs/vmc_hubbard/Main_stripes.x < datasvmc.d > VMC.out
 
 #Read data
-echo "0 1000 5 1" | ../../../../programs/vmc_hubbard/Energy.x  #energy (<H>/N) + error
+echo "0 1000 5 1" | ../../../../programs/vmc_hubbard/Energy.x  #energy**2 (<H**2>/N**2) + error
+echo "<H**2>/N**2: $(tail -n 1 fort.30)"
+mv fort.31 fort.32
+
+echo "0 1000 5 0" | ../../../../programs/vmc_hubbard/Energy.x  #energy (<H>/N) + error
 echo "<H>/N: $(tail -n 1 fort.30)"
 
-echo "0 1000 5 1" | ../../../../programs/vmc_hubbard/Energy.x >#energy**2 (<H**2>/N**2) + error
-echo "<H**2>/N**2: $(tail -n 1 fort.30)"
+../../../../programs/vmc_hubbard/Variance.x
 
 #Note: statistical errors can be reduced by performing more MC steps
