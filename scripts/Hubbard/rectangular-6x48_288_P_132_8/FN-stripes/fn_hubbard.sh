2738ec08a7f6e9846c66c8c5e5d45c9ff4e17128
ltocchio <87852520+ltocchio@users.noreply.github.com>
2024-08-07 12:05:37 +0200

    Create fn_hubbard.sh

---
+++ scripts/Hubbard/rectangular-6x48_288_P_132_8/FN-stripes/fn_hubbard.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+#Compile codes
+cd ../../../../programs/vmc_hubbard || exit
+chmod +x compile.sh
+./compile.sh
+cd -
+
+#Run VMC
+../../../../programs/vmc_hubbard/Prep_stripes_wf.x < datprep.d > prep.out
+../../../../programs/vmc_hubbard/Main_stripes_wf.x < datasfn.d > FN.out
+
+#Read data
+
+echo "30 500 2 0" | ../../../../programs/vmc_hubbard/Energy.x  #energy (<H>/N) + error
+echo "<H>/N: $(tail -n 1 fort.30)"
+
+#Note: statistical errors can be reduced by performing more MC steps
