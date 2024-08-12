2b03aebb446e6beed172d4dfe1931dab593e2d9d
Mohamed Hibat-Allah <45025650+mhibatallah@users.noreply.github.com>
2024-08-12 11:30:31 -0400

    Update dmrg.sh
    Adding ITensor C++

--- scripts/Heisenberg/triangular_144_O/dmrg.sh
+++ scripts/Heisenberg/triangular_144_O/dmrg.sh
@@ -2,3 +2,7 @@
 
 cd programs/dmrg_itensors || exit
 julia --project heisenberg_2d.jl --L 12 --J2 1 --zero_mag --max_B 1024 --seed 123
+
+cd programs/dmrg_itensors_c++ || exit
+g++ dmrg_triangular_heisenberg_12x12.cc -o dmrg_triangular_heisenberg_12x12
+./dmrg_triangular_heisenberg_12x12
